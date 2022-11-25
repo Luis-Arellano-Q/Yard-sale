5339b5ee28af97287ce79217e0f9c41dcc298172
Luis-Arellano-Q <arellano_18angel@hotmail.com>
2022-11-25 17:44:05 -0500

    nuevos cambios en la convivencia de bloques de codigo en la experiencia del usuario

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const hamMenu = document.querySelector('.nav-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const productDetailCLose = document.querySelector('.product-detail-close-secondary');
 const menuCarIcon = document.querySelector('.car');
 const aside = document.querySelector('.product-detail');
+const asideSecondary = document.querySelector('.product-detail-secondary');
 
 // const mainContainer = document.querySelector(".main");
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 hamMenu.addEventListener('click',toggleMobileMenu);
 menuCarIcon.addEventListener('click',toggleCarritoMenu);
+productDetailCLose.addEventListener('click', closeProductDetailAside)
 
 // mainContainer.addEventListener('click', colseMenusClick);
 
@@ -33,52 +36,113 @@ menuCarIcon.addEventListener('click',toggleCarritoMenu);
 
 // }
 
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    descrip: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet necessitatibus dolore error voluptatibus corporis unde harum quis illo accusamus, assumenda mollitia consequuntur expedita aliquid, sapiente sit odio, dolorum veritatis possimus!'
+});
+productList.push({
+    name: 'Laptop',
+    price: 1200,
+    image:'https://m.media-amazon.com/images/I/71RK6+rx-xL.jpg',
+    descrip: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet necessitatibus dolore error voluptatibus corporis unde harum quis illo accusamus, assumenda mollitia consequuntur expedita aliquid, sapiente sit odio, dolorum veritatis possimus!'
+});
+productList.push({
+    name: 'phone',
+    price: 700,
+    image:'https://image.made-in-china.com/155f0j00UhrpdSDRVlGT/Cell-Phone-HYT-Mate48-PRO-5-5-Inch-Phone-Smart-Phone-Manufacturers-Direct-Selling-Mobile-Android-Phone.jpg',
+    descrip: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet necessitatibus dolore error voluptatibus corporis unde harum quis illo accusamus, assumenda mollitia consequuntur expedita aliquid, sapiente sit odio, dolorum veritatis possimus!'
+});
+productList.push({
+    name: 'headphones',
+    price: 150,
+    image:'https://img.freepik.com/free-photo/pink-headphones-wireless-digital-device_53876-96804.jpg?w=2000',
+    descrip: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet necessitatibus dolore error voluptatibus corporis unde harum quis illo accusamus, assumenda mollitia consequuntur expedita aliquid, sapiente sit odio, dolorum veritatis possimus!'
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    descrip: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet necessitatibus dolore error voluptatibus corporis unde harum quis illo accusamus, assumenda mollitia consequuntur expedita aliquid, sapiente sit odio, dolorum veritatis possimus!'
+});
+productList.push({
+    name: 'Laptop',
+    price: 1200,
+    image:'https://m.media-amazon.com/images/I/71RK6+rx-xL.jpg',
+    descrip: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet necessitatibus dolore error voluptatibus corporis unde harum quis illo accusamus, assumenda mollitia consequuntur expedita aliquid, sapiente sit odio, dolorum veritatis possimus!'
+});
+productList.push({
+    name: 'phone',
+    price: 700,
+    image:'https://image.made-in-china.com/155f0j00UhrpdSDRVlGT/Cell-Phone-HYT-Mate48-PRO-5-5-Inch-Phone-Smart-Phone-Manufacturers-Direct-Selling-Mobile-Android-Phone.jpg',
+    descrip: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet necessitatibus dolore error voluptatibus corporis unde harum quis illo accusamus, assumenda mollitia consequuntur expedita aliquid, sapiente sit odio, dolorum veritatis possimus!'
+});
+productList.push({
+    name: 'headphones',
+    price: 150,
+    image:'https://img.freepik.com/free-photo/pink-headphones-wireless-digital-device_53876-96804.jpg?w=2000',
+    descrip: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet necessitatibus dolore error voluptatibus corporis unde harum quis illo accusamus, assumenda mollitia consequuntur expedita aliquid, sapiente sit odio, dolorum veritatis possimus!'
+});
+
 function toggleDesktopMenu() {
     aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
+    asideSecondary.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
     aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+    asideSecondary.classList.add('inactive');
 }
 function toggleCarritoMenu() {
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
+    asideSecondary.classList.add('inactive');
     aside.classList.toggle('inactive');
 }
+function showAsideSecondary(id) {
+    const asideSecImg = document.querySelector('.asideSecImg');  
+    const asidePrice = document.querySelector('.asidePrice');
+    const asideNameProduct = document.querySelector('.asideNameProduct');
+    const asideDescrip = document.querySelector('.asideDescrip');
+    asideSecImg.setAttribute('src',productList[id].image);
+    asidePrice.innerText= '$'+ productList[id].price;
+    asideNameProduct.innerText=productList[id].name;
+    asideDescrip.innerText=productList[id].descrip;
+}
 
+function openProductDetail(event) {
+    asideSecondary.classList.remove('inactive');
+    desktopMenu.classList.add('inactive');
+    aside.classList.add('inactive');
+    
+    // const asideImgValue = document.querySelector('.imgValue');
+    // asideImgValue.addEventListener('click',showAsideSecondary);
+    // const id = asideImgValue.getAttribute('name');
 
-const productList = [];
-productList.push({
-    name: 'Bike',
-    price: 120,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'Laptop',
-    price: 1200,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-productList.push({
-    name: 'phone',
-    price: 700,
-    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-});
-
+    const id = Math.round(Math.random()*8);
+    showAsideSecondary(id);
+}
+function closeProductDetailAside() {
+    asideSecondary.classList.add('inactive');
+}
 
 function renderProduct(arr) {
+    let i = 0;
     for (const product of arr) {
         const cardsContainer = document.querySelector('.cards-conteiner');
 
-        
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
         
-        
-        const img= document.createElement('img');
-        img.setAttribute('src',product.image);
-        
+        const productImg= document.createElement('img');
+        productImg.setAttribute('name',i++);
+        productImg.classList.add('imgValue');
+        productImg.setAttribute('src',product.image);
+        productImg.addEventListener('click', openProductDetail);
         
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
@@ -100,7 +164,7 @@ function renderProduct(arr) {
         productInfoFigure.appendChild(productImgCart);
         
         productInfo.append(productInfoDiv,productInfoFigure);
-        productCard.append(img,productInfo);
+        productCard.append(productImg,productInfo);
         cardsContainer.appendChild(productCard);
     }
     
